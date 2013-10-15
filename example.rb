d37f4768281600e374229146526ef8e9559ce437
James W. Brinkerhoff <jwb@paravolve.net>
2013-10-15 00:41:13 -0400

    updated example DSL script

--- example.rb
+++ example.rb
@@ -1,17 +1,18 @@
-#!/usr/bin/env ruby
+table "filter", { logging: false, flush: false } do
+  # host_list "admin_hosts", { set_policy: false, create: false } do
+  #   hosts %w{ some.hosts.here and.others.like 1.2.3.4 and 2000:1234:100::/64 }
+  #   type 'source'
+  # end
 
-$:.unshift File.dirname(__FILE__) + "/lib"
+  chain "TESTA", { create: false, set_policy: false } do
+    %w{ cerberus0 }.each do |int|
+      rule "allow traffic between #{int} and eth0" do
+        in_interface  int
+        out_interface 'eth0'
+        type          :IPV6
 
-require "paravolve/custom_firewall"
-STDOUT.sync = true
-
-fw = ParaVolve::CustomFirewall.configure "example", { setup: false } do
-  table "filter", { logging: false, flush: false } do
-    host_list "admin_hosts", { set_policy: false, create: false } do
-      hosts %w{ some.hosts.here and.others.like 1.2.3.4 and 2000:1234:100::/64 }
-      type 'source'
+        accept
+      end
     end
-	end
+  end
 end
-
-puts fw
