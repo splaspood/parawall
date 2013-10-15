283852551e9135ba39ca36be48292c64bb77a148
James W. Brinkerhoff <jwb@paravolve.net>
2013-10-14 21:25:39 -0400

    converted run to use thor, DSL files now contain only DSL from table level down

--- lib/paravolve/custom_firewall.rb
+++ lib/paravolve/custom_firewall.rb
@@ -3,14 +3,14 @@ require "paravolve/firewall_builder/host_list"
 
 module ParaVolve
   module CustomFirewall
-    def self.configure(name, options = { debug: false }, &block)
+    def self.configure(name, options = { debug: false }, block)
       str  = "#!/bin/bash\n"
       str += "set -x\n" if options.has_key?(:debug) and options[:debug]
 
 			str += "# Generated: #{Time.now}\n"
 
       fw = FirewallBuilder::Firewall.new(name)
-      fw.instance_eval(&block)
+      fw.instance_eval(block, name)
 			str += fw.to_s
 
       str
